89c558400b0f4c2d2ad576b551b50ee5f67b3f78
Jackson Teixeira <jackson.s.teixeira@gmail.com>
2019-09-24 11:03:05 -0300

    Professional

--- lib/repositories/professionals_repository.dart
+++ lib/repositories/professionals_repository.dart
@@ -1,12 +1,31 @@
+import 'dart:io';
+
 import 'package:callma/models/professional.dart';
 import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfessionalsRepository {
   static Future<List<Professional>> getProfessionals(int professionId) async {
     List<Professional> items = new List<Professional>();
 
     try {
-      Response response = await Dio().get("https://callma-api.herokuapp.com/api/professionals");
+      //
+      // Token
+      //
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String token = await prefs.get("token");
+
+      //
+      // Request
+      //
+      Dio dio = new Dio();
+      dio.options.headers = {'Authorization': 'Bearer ' + token};
+      dio.options.contentType = ContentType.parse("application/json");
+
+      //
+      // Response
+      //
+      Response response = await dio.get("https://callma-api.herokuapp.com/api/professionals");
 
       for (Map<String, dynamic> item in response.data) {
         items.add(Professional.fromJson(item));
